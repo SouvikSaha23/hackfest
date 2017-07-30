e6ea8ab39ae69b4c9bc7056a7c2afff40bc7c7a0
SouvikSaha23 <biswajitsahaicst@gmail.com>
2017-07-30 23:55:12 +0530

    Connecting to database of users

--- Hackfest/php_files/signup_access.php
+++ Hackfest/php_files/signup_access.php
@@ -6,7 +6,7 @@
       $conn = new PDO("mysql:host=$servername;dbname=SignUp", $username, $password);
       // set the PDO error mode to exception
       $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    //  echo "Connected successfully<br>";
+     //  echo "Connected successfully<br>";
       }
   catch(PDOException $e)
       {
