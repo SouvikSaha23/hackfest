485f39c6a6bc6f96b66b90d9ba698dec9944aa48
SouvikSaha23 <biswajitsahaicst@gmail.com>
2017-07-30 23:55:58 +0530

    Inserting new users in database

--- Hackfest/php_files/signup_entry.php
+++ Hackfest/php_files/signup_entry.php
@@ -5,10 +5,10 @@
     $phone= $_POST['mobile'];
     $address= $_POST['address'];
     $pass= $_POST['password'];
-//require_once allows to access the contents of other php file
-//in the current php file. This saves us from writing the same code here...
+  //require_once allows to access the contents of other php file
+  //in the current php file. This saves us from writing the same code here...
     require_once('signup_access.php');
-//It is basically calling the whole code of signup access here
+  //It is basically calling the whole code of signup access here
     $sql= $conn->query("INSERT INTO usr_details(Name,Email,Address,Phone,Password) VALUES ('$name','$email','$address','$phone','$pass')");
     if($sql){
       echo "Successfully Registered";
